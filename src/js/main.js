cf26b763f1ed9791991484eca79f818497abd2b8
Irina <irensaywhen@gmail.com>
2020-07-31 09:14:31 +0300

    Event handlers set

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 if (window.WebSocket) {
   const socket = new WebSocket("ws://echo.websocket.org");
   const label = document.getElementById("status-label");
+  const textView = document.getElementById("text-view");
+  const buttonSend = document.getElementById("send-button");
+  const buttonStop = document.getElementById("stop-button");
 
   socket.onopen = function (event) {
     label.innerHTML = "Connection established";
@@ -8,6 +11,34 @@ if (window.WebSocket) {
 
   socket.onmessage = function (event) {
     label.innerHTML = event.data;
+  };
+
+  socket.onclose = function (event) {
+    console.log("Connection closed");
+
+    let { code, reason, wasClean } = event;
+
+    wasClean
+      ? (label.innerHTML = "Connection closed normally")
+      : (label.innerHTML = `Connection closed with message ${reason}. Code: ${code}`);
+  };
+
+  socket.onerror = function (event) {
+    console.log("Error occured");
+
+    label.innerHTML = `Error: ${event}`;
+  };
+
+  buttonSend.onclick = function () {
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.send(textView.value);
+    }
+  };
+
+  buttonStop.onclick = function () {
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.close(1000, "Deliberate disconnection");
+    }
   };
 } else {
   console.log("WebSockets are not supported");
